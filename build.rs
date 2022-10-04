39abc64e7b4435592e774fdfc5d73b3c87f8475d
Marco Munizaga <git@marcopolo.io>
2022-10-04 10:19:24 -0700

    Add proto3 optional example

--- build.rs
+++ build.rs
@@ -2,5 +2,6 @@ use std::io::Result;
 fn main() -> Result<()> {
     prost_build::compile_protos(&["src/example3.proto"], &["src/"])?;
     prost_build::compile_protos(&["src/example2.proto"], &["src/"])?;
+    prost_build::compile_protos(&["src/example3optional.proto"], &["src/"])?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ fn main() {
 pub mod example3 {
     include!(concat!(env!("OUT_DIR"), "/example3.rs"));
 }
+pub mod example3optional {
+    include!(concat!(env!("OUT_DIR"), "/example3optional.rs"));
+}
 
 pub mod example2 {
     include!(concat!(env!("OUT_DIR"), "/example2.rs"));
@@ -106,6 +109,68 @@ mod tests {
 
         // But we should be able to interpret them regardless of proto2 vs proto3
         let b2to3 = example3::NoiseHandshakePayload::decode(bytes::Bytes::from(b2))
+            .expect("should decode bytes from 2");
+        assert_eq!(b2to3, hs3);
+
+        let b3to2 = example2::NoiseHandshakePayload::decode(bytes::Bytes::from(b3))
+            .expect("should decode bytes from 3");
+
+        assert_eq!(b3to2, hs2);
+    }
+
+    #[test]
+    fn empty_bytes_proto3_optional() {
+        let hs2 = example2::NoiseHandshakePayload {
+            identity_key: Some(vec![]),
+            identity_sig: Some(vec![]),
+            extensions: None,
+        };
+        let b2 = hs2.encode_to_vec();
+
+        let hs3 = example3optional::NoiseHandshakePayload {
+            identity_key: Some(vec![]),
+            identity_sig: Some(vec![]),
+            extensions: None,
+        };
+        let b3 = hs3.encode_to_vec();
+
+        // These are the same on the wire
+        assert_eq!(b2, b3);
+        assert_eq!(b2, vec![10, 0, 18, 0]);
+
+        // Round trip is fine
+        let b2to3 = example3optional::NoiseHandshakePayload::decode(bytes::Bytes::from(b2))
+            .expect("should decode bytes from 2");
+        assert_eq!(b2to3, hs3);
+
+        let b3to2 = example2::NoiseHandshakePayload::decode(bytes::Bytes::from(b3))
+            .expect("should decode bytes from 3");
+
+        assert_eq!(b3to2, hs2);
+    }
+
+    #[test]
+    fn unset_bytes_proto3_optional() {
+        let hs2 = example2::NoiseHandshakePayload {
+            identity_key: None,
+            identity_sig: None,
+            extensions: None,
+        };
+        let b2 = hs2.encode_to_vec();
+
+        let hs3 = example3optional::NoiseHandshakePayload {
+            identity_key: None,
+            identity_sig: None,
+            extensions: None,
+        };
+        let b3 = hs3.encode_to_vec();
+
+        // These are the same on the wire
+        assert_eq!(b2, b3);
+        assert_eq!(b2, vec![]);
+
+        // Round trip is fine
+        let b2to3 = example3optional::NoiseHandshakePayload::decode(bytes::Bytes::from(b2))
             .expect("should decode bytes from 2");
         assert_eq!(b2to3, hs3);
 
